da564c287a479b664dc76338f4e2766c896eb972
Vamsi krishna Muktevi <vamsikrishnamuktevi@Vamsis-MacBook-Air.local>
2019-12-26 13:39:47 +0530

    Initial code checkin

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import app from './app';
+import * as dotenv from 'dotenv';
+dotenv.config();
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(process.env.PORT)
+    console.log('App is listing on port ' + PORT);
+});
